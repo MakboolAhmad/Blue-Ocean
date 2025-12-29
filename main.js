ecbdc65b4cfba00446e6ad23e8fb73e84525c9cf
MakboolAhmad <47457274+MakboolAhmad@users.noreply.github.com>
2025-12-29 17:25:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
+const all_exceptions_filter_1 = require("./common/filters/all-exceptions.filter");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
+    }));
+    app.useGlobalFilters(new all_exceptions_filter_1.AllExceptionsFilter());
+    app.setGlobalPrefix('api/v1');
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application running on port ${port}`);
+    console.log(`API available at http://localhost:${port}/api/v1`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
